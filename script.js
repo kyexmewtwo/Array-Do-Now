ad7422a13752a70ce0a4a3acb36641c248ccc0f9
kyexmewtwo <45476037+kyexmewtwo@users.noreply.github.com>
2019-04-29 09:20:57 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/* CAN YOU ADD ADDITIONAL VEGETABLES TO THE PAGE? */
+var vegetables = ["broccoli","carrots", "celery", "beats", "radish", "yam"];
+
+
+
+
+
+
+/* DO NOT CHANGE BEYOND THIS LINE */
+$('body').append( vegetables.join(", ") );
